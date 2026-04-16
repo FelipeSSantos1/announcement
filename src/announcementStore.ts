774bcf174b512a6de49a5a54924b54124d95094a
Felipe S. Santos <felipe.souzasantos@hingehealth.com>
2026-04-16 12:55:45 -0600

    feat: track read announcement ids in globalState

---
+++ src/announcementStore.ts
@@ -0,0 +1,32 @@
+import * as vscode from 'vscode';
+
+const READ_IDS_KEY = 'announcements.readIds';
+
+export class AnnouncementStore {
+  constructor(private readonly memento: vscode.Memento) {}
+
+  private readIds(): Set<number> {
+    return new Set<number>(this.memento.get<number[]>(READ_IDS_KEY, []));
+  }
+
+  isRead(id: number): boolean {
+    return this.readIds().has(id);
+  }
+
+  async markRead(id: number): Promise<void> {
+    const ids = this.readIds();
+    ids.add(id);
+    await this.memento.update(READ_IDS_KEY, Array.from(ids));
+  }
+
+  async markAllRead(ids: number[]): Promise<void> {
+    const current = this.readIds();
+    ids.forEach((i) => current.add(i));
+    await this.memento.update(READ_IDS_KEY, Array.from(current));
+  }
+
+  unreadOf(ids: number[]): number[] {
+    const read = this.readIds();
+    return ids.filter((i) => !read.has(i));
+  }
+}
